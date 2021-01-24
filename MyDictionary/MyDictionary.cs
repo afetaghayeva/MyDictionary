25bde13a4955c777e7ab5d4fcd0ca6efd6389266
afetagayeva.2003@gmail.com <afetagayeva.2003@gmail.com>
2021-01-24 18:38:44 +0400

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        private TKey[] _key;
+        private TValue[] _value;
+
+        private TKey[] _tempKeys;
+        private TValue[] _tempValues;
+
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            _tempKeys = _key;
+            _key = new TKey[_key.Length + 1];
+            _tempValues = _value;
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _tempValues.Length; i++)
+            {
+                _key[i] = _tempKeys[i];
+                _value[i] = _tempValues[i];
+            }
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+        public TKey[] Keys
+        {
+            get
+            {
+                return _key;
+            }
+        }
+
+        public TValue[] Values
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _key.Length;
+            }
+        }
+
+        private TKey tempKey;
+        private TValue tempValue;
+        public void Insert(int index, TKey key, TValue value)
+        {
+            _tempKeys = _key;
+            _key = new TKey[_key.Length + 1];
+            _tempValues = _value;
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _key.Length; i++)
+            {
+                if (i == index)
+                {
+                    _key[i] = key;
+                    _value[i] = value;
+                    
+                }
+                else if (i>index)
+                {
+                    _key[i] = _tempKeys[i - 1];
+                    _value[i] = _tempValues[i - 1];
+                }
+                else
+                {
+                    _key[i] = _tempKeys[i];
+                    _value[i] = _tempValues[i];
+                }
+            }
+        }
+
+        public void DeleteByKey(TKey key)
+        {
+            _tempKeys = _key;
+            _key = new TKey[_key.Length - 1];
+            for (int i = 0; i < _key.Length; i++)
+            {
+                if (i >= Array.IndexOf(_tempKeys, key))
+                {
+                    _key[i] = _tempKeys[i + 1];
+                }
+                else
+                {
+                    _key[i] = _tempKeys[i];
+                }
+                
+            }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+            dictionary.Add("table","masa");
+            dictionary.Add("book","kitab");
+            dictionary.Add("student","telebe");
+           // dictionary.Insert(2,"teacher","muellim");
+           dictionary.DeleteByKey("student");
+            Console.WriteLine(dictionary.Count);
+            foreach (var dictionaryKey in dictionary.Keys)
+            {
+                Console.WriteLine(dictionaryKey);
+            }
+
+
+        }
+    }
+}
